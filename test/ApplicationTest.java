46d341f890512ed3b191b4c409a6368d9e3dd56b
Siva Nookala <siva.nookala@cerone.co.in>
2016-10-21 12:54:42 +0530

    Added the framework for saving the User and the signup feature.

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,45 +1,31 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import controllers.HomeController;
+import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.mvc.Result;
+import play.test.WithApplication;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.contentAsString;
 
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-import play.twirl.api.Content;
+public class ApplicationTest extends WithApplication {
 
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
-
-
-/**
- *
- * Simple (JUnit) tests that can call all parts of a play app.
- * If you are interested in mocking a whole application, see the wiki for more details.
- *
- */
-public class ApplicationTest {
-
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertEquals(2, a);
+    @Override
+    protected Application provideApplication() {
+        return new GuiceApplicationBuilder()
+                .configure("play.http.router", "router.Routes")
+                .build();
     }
 
     @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertEquals("text/html", html.contentType());
-        assertTrue(html.body().contains("Your new application is ready."));
+    public void testIndex() {
+        Result result = new HomeController().index();
+        assertEquals(OK, result.status());
+        assertEquals("text/html", result.contentType().get());
+        assertEquals("utf-8", result.charset().get());
+        assertTrue(contentAsString(result).contains("11Welcome234"));
     }
-
 
 }
