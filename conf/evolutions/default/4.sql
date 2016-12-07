6825e6535962bfcee3ec7fd2ee0efe1bc839d373
vedanath <valluruvedanath@gmail.com>
2016-12-07 16:16:40 +0530

    Fixed the issue with the evolution of old data.

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -1,8 +1,8 @@
 
 # --- !Ups
 
-alter table user alter column mobile_verified boolean default 'false';
-alter table user alter column is_ride_in_progress boolean default 'false';
+update user set mobile_verified = 'false' where mobile_verified is null;
+update user set is_ride_in_progress = 'false' where is_ride_in_progress is null;
 
 # --- !Downs
 
