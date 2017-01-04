2e82ba59698eaa6266d29fa50440ac38f01bd4ea
Siva Nookala <siva.nookala@cerone.co.in>
2017-01-04 19:17:12 +0530

    Changed the type to raw form instead of json.

--- app/controllers/PaymentController.java
+++ app/controllers/PaymentController.java
@@ -1,10 +1,13 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.jetbrains.annotations.NotNull;
 import play.mvc.BodyParser;
 import play.mvc.Result;
 import views.html.payUSuccess;
 import views.html.payUFailure;
+
+import java.util.Map;
 
 import static play.mvc.Controller.request;
 
@@ -14,14 +17,29 @@ import static play.mvc.Controller.request;
 public class PaymentController extends BaseController{
     public Result payUSuccess() {
         logRequest();
-        JsonNode locationsJson = request().body().asJson();
-        return ok(payUSuccess.render(locationsJson.toString()));
+        return ok(payUSuccess.render(getFormAsString()));
+    }
+
+    @NotNull
+    private String getFormAsString() {
+        Map<String, String[]> formUrlEncoded = request().body().asFormUrlEncoded();
+        String response = "";
+        for(String key : formUrlEncoded.keySet())
+        {
+            String values[] = formUrlEncoded.get(key);
+            response += key + " : ";
+            for(String value : values)
+            {
+                response += value +", ";
+            }
+            response += "\n";
+        }
+        return response;
     }
 
     public Result payUFailure() {
         logRequest();
-        JsonNode locationsJson = request().body().asJson();
-        return ok(payUFailure.render(locationsJson.toString()));
+        return ok(payUFailure.render(getFormAsString()));
     }
 
     private void logRequest() {
