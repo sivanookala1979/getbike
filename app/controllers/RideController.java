6381c91ec36a17056b1cf3bb0c8dab19d061e00f
ramkoti <ramkoti@vave.co.in>
2017-10-16 15:36:39 +0530

    changes from ram: 1.changing callhealth api url from dev to preprod.

--- app/controllers/RideController.java
+++ app/controllers/RideController.java
@@ -1522,7 +1522,7 @@ public class RideController extends BaseController {
         ActorMaterializer materializer = ActorMaterializer.create(settings, system, name);
         WSClient client = new AhcWSClient(config, materializer);
         //Call health url https://medicines.callhealthshop.com/MZIMRestServices/v1/postMZIMOrderStatus and change the RequestorId to 2429
-        client.url("https://medicines-uat.callhealthshop.com/MZIMRestServices/v1/postMZIMOrderStatus").post(jsonNode).whenComplete((r, e) -> {
+        client.url("https://medicines-preprod.callhealthshop.com/MZIMRestServices/v1/postMZIMOrderStatus").post(jsonNode).whenComplete((r, e) -> {
             System.out.println("++++++++++++++++++++++++++++"+r.getBody());
         }).thenRun(() -> {
             try {
